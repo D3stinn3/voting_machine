f7083e20642847b6004bf12c45f8ea03ff651347
D3stinn3 <paynedestinne@gmail.com>
2024-05-14 22:20:39 +0300

    Voting Contract

--- projects/voting_machine/smart_contracts/voting_machine/contract.py
+++ projects/voting_machine/smart_contracts/voting_machine/contract.py
@@ -1,7 +1,258 @@
-from algopy import ARC4Contract, arc4
+import typing
+
+from algopy import (
+    ARC4Contract,
+    Bytes,
+    Global,
+    GlobalState,
+    OpUpFeeSource,
+    String,
+    Txn,
+    UInt64,
+    arc4,
+    ensure_budget,
+    gtxn,
+    itxn,
+    log,
+    op,
+    subroutine,
+    uenumerate,
+    urange,
+)
+
+VoteIndexArray: typing.TypeAlias = arc4.DynamicArray[arc4.UInt8]
+
+VOTE_INDEX_BYTES = 1
+VOTE_COUNT_BYTES = 8
+
+# The minimum balances for increment for: each box created, each byte of boxes(key included) and each asset opted into
+BOX_FLAT_MIN_BALANCE = 2500
+BOX_BYTE_MIN_BALANCE = 400
+ASSET_MIN_BALANCE = 100000
+
+TALLY_BOX_KEY = b"V"
 
 
-class VotingMachine(ARC4Contract):
-    @arc4.abimethod()
-    def hello(self, name: arc4.String) -> arc4.String:
-        return "Hello, " + name
+class VotingPreconditions(arc4.Struct):
+    is_voting_open: arc4.UInt64
+    is_allowed_to_vote: arc4.UInt64
+    has_already_voted: arc4.UInt64
+    current_time: arc4.UInt64
+
+
+class VotingRoundApp(ARC4Contract):
+    def __init__(self) -> None:
+        self.is_bootstrapped = False
+
+        # Minimum number of voters who voted
+        self.voter_count = UInt64(0)
+        self.close_time = GlobalState(UInt64)
+
+    @arc4.abimethod(create="require")
+    def create(
+        self,
+        vote_id: String,
+        snapshot_public_key: Bytes,
+        metadata_ipfs_cid: String,
+        start_time: UInt64,
+        end_time: UInt64,
+        option_counts: VoteIndexArray,
+        quorum: UInt64,
+        nft_image_url: String,
+    ) -> None:
+        assert start_time < end_time
+        assert end_time >= Global.latest_timestamp
+
+        self.vote_id = vote_id
+        self.snapshot_public_key = snapshot_public_key
+        self.metadata_ipfs_cid = metadata_ipfs_cid
+        self.start_time = start_time
+        self.end_time = end_time
+        self.quorum = quorum
+        self.nft_image_url = nft_image_url
+        self.store_option_counts(option_counts.copy())
+
+    @arc4.abimethod
+    def bootstrap(self, fund_min_bal_req: gtxn.PaymentTransaction) -> None:
+        # Certain that not bootstrapped
+        assert not self.is_bootstrapped
+
+        self.is_bootstrapped = True
+
+        assert fund_min_bal_req.receiver == Global.current_application_address
+
+        tally_box_size = self.total_options * VOTE_COUNT_BYTES
+        min_balance_required = (
+            # minimum balance required for: ALGO + Vote result NFT asset
+            ASSET_MIN_BALANCE * 2
+            # create NFT fee
+            + 1000
+            # tally box
+            + BOX_FLAT_MIN_BALANCE
+            # tally box key
+            + (tally_box_size * BOX_BYTE_MIN_BALANCE)
+        )
+
+        log(min_balance_required)
+        assert fund_min_bal_req.amount == min_balance_required
+
+        assert op.Box.create(TALLY_BOX_KEY, tally_box_size)
+
+    @arc4.abimethod
+    def close(self) -> None:
+        ensure_budget(20000, fee_source=OpUpFeeSource.GroupCredit)
+
+        assert not self.close_time
+        self.close_time.value = Global.latest_timestamp
+
+        note = (
+            '{"standard" : "arc69",'
+            '"description": "This is a voting result NFT for voting round with ID '
+            + self.vote_id
+            + '.", "properties": {"metadata": ipfs://'
+            + self.metadata_ipfs_cid
+            + '","id": "'
+            + self.vote_id
+            + itoa(self.quorum)
+            + ',"voterCount":'
+            + itoa(self.voter_count)
+            + ', "tallies": ['
+        )
+
+        current_index = UInt64(0)
+
+        for question_index, question_options in uenumerate(self.option_counts):
+            if question_index > 0:
+                note += ","
+            if question_options > 0:
+                note += "["
+                for option_index in urange(question_options.native):
+                    if option_index > 0:
+                        note += ","
+                    votes_for_option = get_vote_from_box(current_index)
+                    note += itoa(votes_for_option)
+                    current_index += 1
+                note += "]"
+        note += "]}}"
+        self.nft_asset_id = (
+            itxn.AssetConfig(
+                total=1,
+                decimals=0,
+                default_frozen=False,
+                asset_name="[VOTE RESULT] " + self.vote_id,
+                unit_name="VOTERSLT",
+                url=self.nft_image_url,
+                note=note,
+            )
+            .submit()
+            .created_asset.id
+        )
+
+    @arc4.abimethod(readonly=False)
+    def get_preconditions(self, signature: Bytes):
+        return VotingPreconditions(
+            is_voting_open=arc4.UInt64(self.voting_open()),
+            is_allowed_to_vote=arc4.UInt64(self.allowed_to_vote(signature)),
+            has_already_voted=arc4.UInt64(self.already_voted()),
+            current_time=arc4.UInt64(Global.latest_timestamp),
+        )
+
+    @arc4.abimethod
+    def vote(
+        self,
+        fund_min_bal_req: gtxn.PaymentTransaction,
+        signature: Bytes,
+        answer_ids: VoteIndexArray,
+    ) -> None:
+        ensure_budget(7700, fee_source=OpUpFeeSource.GroupCredit)
+
+        # Check voting preconditions
+        assert self.allowed_to_vote(signature)
+        assert self.voting_open()
+        assert not self.already_voted()
+        questions_count = self.option_counts.length
+        assert answer_ids.length == questions_count
+
+        # Check if voter box is funded
+        min_bal_req = BOX_FLAT_MIN_BALANCE + (
+            (32 + 2 + VOTE_INDEX_BYTES * answer_ids.length) * BOX_BYTE_MIN_BALANCE
+        )
+
+        assert fund_min_bal_req.receiver == Global.current_application_address
+
+        log(min_bal_req)
+        assert fund_min_bal_req.amount == min_bal_req
+
+        # Record vote for each question
+        cumulative_offset = UInt64(0)
+
+        for question_index in urange(questions_count):
+
+            # Load the users vote for this question
+            answer_option_index = answer_ids[question_index].native
+            options_count = self.option_counts[question_index].native
+            assert answer_option_index < options_count
+            increment_vote_in_box(cumulative_offset + answer_option_index)
+            cumulative_offset += options_count
+            op.Box.put(Txn.sender.bytes, answer_ids.bytes)
+            self.voter_count += 1
+
+    @subroutine
+    def voting_open(self) -> bool:
+        return (
+            self.is_bootstrapped
+            and not self.close_time
+            and self.start_time <= Global.latest_timestamp <= self.end_time
+        )
+
+    @subroutine
+    def already_voted(self) -> bool:
+        (votes, exists) = op.Box.get(Txn.sender.bytes)
+        return exists
+
+    @subroutine
+    def store_option_counts(self, option_counts: VoteIndexArray) -> None:
+
+        assert option_counts.length
+        assert option_counts.length <= 112
+
+        total_options = UInt64(0)
+
+        for item in option_counts:
+            total_options += item.native
+
+        assert total_options <= 128
+
+        self.option_counts = option_counts.copy()
+        self.total_options = total_options
+
+    @subroutine
+    def allowed_to_vote(self, signature: Bytes) -> bool:
+        ensure_budget(2000)
+        return op.ed25519verify_bare(
+            Txn.sender.bytes, signature, self.snapshot_public_key
+        )
+
+    @subroutine
+    def get_vote_from_box(index: UInt64) -> UInt64:
+        box_data, exists = op.Box.get(TALLY_BOX_KEY)
+        assert exists
+        return op.btoi(op.extract(box_data, index, VOTE_COUNT_BYTES))
+
+    @subroutine
+    def increment_vote_in_box(index: UInt64) -> None:
+        box_data, exists = op.Box.get(TALLY_BOX_KEY)
+        assert exists
+        current_vote = op.btoi(op.extract(box_data, index, VOTE_COUNT_BYTES))
+
+        op.Box.replace(TALLY_BOX_KEY, index, op.itob(current_vote + 1))
+
+    @subroutine
+    def itoa(i: UInt64) -> String:
+        digits = Bytes(b"0123456789")
+        radix = digits.length
+
+        if i < radix:
+            return String.from_bytes(digits[i])
+
+        return itoa(i // radix) + String.from_bytes(digits[i % radix])
